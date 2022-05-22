5c65335d860bcb124c4f8251cb75c2f364e68d37
PhamThiThaoVan <thaovanpham1010@gmail.com>
2022-05-22 14:34:24 +0700

    TT_HE THONG NHUNG

---
+++ app/src/main/java/pttv/com/example/raspberrypiandmodulesim/MainActivity.java
@@ -0,0 +1,286 @@
+package pttv.com.example.raspberrypiandmodulesim;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    DatabaseReference myData;
+    Button btn_change_phone_number, btn_send_data;
+    TextView tv_hienthi, tv_sdt, tv_vitri;
+    LocationManager locationManager;
+    private static final int RESULT_PICK_CONTACT=1;
+    private static final String CONTENT_TITLE = "Thông báo có rò rỉ khí gas";
+    private static final String CONTENT_TEXT = "Hệ thống nhận được thông báo có rò rỉ khí gas tại phòng ";
+    private int t_so = 1;
+    private int nguong = 400;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_change_phone_number = (Button) findViewById(R.id.change_phone_number);
+        btn_send_data = (Button) findViewById(R.id.send_data);
+        tv_hienthi = (TextView) findViewById(R.id.tv_hienthi);
+        tv_vitri = (TextView) findViewById(R.id.tv_vitri);
+        tv_sdt = (TextView) findViewById(R.id.tv_sdt);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, 100);
+        }
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.READ_CONTACTS
+            }, 100);
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+        myData = FirebaseDatabase.getInstance().getReference();
+
+        myData.child("Analog Value").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                try {
+                    String val_sensor = snapshot.getValue().toString();
+                    Integer val = Integer.parseInt(val_sensor.replaceAll("[\\D]", ""));
+                    int per_val1 = (val * 100) / 1024;
+                    tv_hienthi.setText(per_val1 + "%");
+                    if (val > nguong) {
+                        gui_thong_bao();
+                        myData.child("Status").setValue("SELECT");
+                        Toast.makeText(MainActivity.this, "Đã gửi thông báo vị trí đến người thân", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    tv_hienthi.setText("No Data");
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        myData.child("SDT").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                try {
+                    String sdt = snapshot.getValue().toString();
+                    tv_sdt.setText(sdt);
+                } catch (Exception e) {
+                    tv_sdt.setText("No Data");
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        btn_change_phone_number.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(contactPickerIntent, RESULT_PICK_CONTACT);
+            }
+        });
+
+        btn_send_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myData.child("Status").setValue("SELECT");
+                Toast.makeText(MainActivity.this, "Đã gửi thông báo vị trí đến người thân", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == RESULT_OK) {
+//            switch (requestCode) {
+//                case RESULT_PICK_CONTACT:
+//                    contactPicker(data);
+////                    break;
+//            }
+//        } else {
+//            Toast.makeText(this, "Fail to pick contacts", Toast.LENGTH_SHORT).show();
+//        }
+//    }
+//
+//    private void contactPicker(Intent data) {
+//        Cursor cursor=null;
+//        try{
+//            String phone_no=null;
+//            Uri uri=data.getData();
+//            cursor=getContentResolver().query(uri,null, null,null, null);
+//            int phoneIndex=cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+//            phone_no=cursor.getString(phoneIndex);
+//            tv_sdt.setText(phone_no);
+//        }catch(Exception e){
+//            Toast.makeText(this, "Fail", Toast.LENGTH_SHORT).show();
+//        }
+//    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    Cursor cursor = null;
+                    try {
+                        String phoneNo = null;
+                        String name = null;
+
+                        Uri uri = data.getData();
+                        cursor = getContentResolver().query(uri, null, null, null, null);
+                        cursor.moveToFirst();
+                        int  phoneIndex =cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                        int  nameIndex =cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+                        phoneNo = cursor.getString(phoneIndex);
+                        name = cursor.getString(nameIndex);
+
+                        tv_sdt.setText(phoneNo);
+                        myData.child("SDT").setValue(phoneNo);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        } else {
+            Toast.makeText(this, "Fail to pick contacts", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void gui_thong_bao() {
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Notification thong_bao=new NotificationCompat.Builder(this, MyNotification.CHANNEL_ID)
+                .setContentTitle(CONTENT_TITLE)
+                .setContentText(CONTENT_TEXT)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(CONTENT_TEXT))
+                .setSmallIcon(R.drawable.logo)
+                .setSound(uri)
+                .setLargeIcon(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.icon_bao_dong), 128, 128, false))
+                .build();
+
+        NotificationManager notificationManager=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if(notificationManager!=null) notificationManager.notify(t_so, thong_bao);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        long longitude=(long) location.getLongitude();
+        long latitude=(long) location.getLatitude();
+//        tv_vitri.setText("Kinh độ: "+longitude+"  Vĩ độ: "+latitude);
+
+        try {
+            Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+            String address = addresses.get(0).getAddressLine(0);
+            String utf_address = convert(address);
+
+            tv_vitri.setText(address);
+            myData.child("Location").setValue(utf_address);
+
+        }catch (Exception e){
+            tv_vitri.setText("No data");
+        }
+    }
+
+    public static String convert(String str) {
+        str = str.replaceAll("à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ", "a");
+        str = str.replaceAll("è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ", "e");
+        str = str.replaceAll("ì|í|ị|ỉ|ĩ", "i");
+        str = str.replaceAll("ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ", "o");
+        str = str.replaceAll("ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ", "u");
+        str = str.replaceAll("ỳ|ý|ỵ|ỷ|ỹ", "y");
+        str = str.replaceAll("đ", "d");
+
+        str = str.replaceAll("À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ", "A");
+        str = str.replaceAll("È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ", "E");
+        str = str.replaceAll("Ì|Í|Ị|Ỉ|Ĩ", "I");
+        str = str.replaceAll("Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ", "O");
+        str = str.replaceAll("Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ", "U");
+        str = str.replaceAll("Ỳ|Ý|Ỵ|Ỷ|Ỹ", "Y");
+        str = str.replaceAll("Đ", "D");
+        return str;
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        LocationListener.super.onStatusChanged(provider, status, extras);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
+    }
+}
